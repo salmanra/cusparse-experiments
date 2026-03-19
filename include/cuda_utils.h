c31359de38186521daa9a45463c12f96d400d3df
Rahmy Salman <rahmysalman@gmail.com>
2026-03-19 15:39:52 -0400

    gemm code for comparisons

--- include/cuda_utils.h
+++ include/cuda_utils.h
@@ -3,6 +3,7 @@
 
 #include <cuda_runtime.h>
 #include <cusparse.h>
+#include <cublas_v2.h>
 #include <cstdio>
 #include <cstdlib>
 
@@ -27,6 +28,33 @@
             exit(EXIT_FAILURE);                                               \
         }                                                                     \
     } while (0)
+
+// cuBLAS error checking macro
+#define CHECK_CUBLAS(call)                                                    \
+    do {                                                                      \
+        cublasStatus_t status = call;                                         \
+        if (status != CUBLAS_STATUS_SUCCESS) {                                \
+            fprintf(stderr, "cuBLAS error at %s:%d: %s\n",                    \
+                    __FILE__, __LINE__, cublasGetErrorString(status));        \
+            exit(EXIT_FAILURE);                                               \
+        }                                                                     \
+    } while (0)
+
+// Helper function to get cuBLAS error string
+inline const char* cublasGetErrorString(cublasStatus_t status) {
+    switch (status) {
+        case CUBLAS_STATUS_SUCCESS:          return "CUBLAS_STATUS_SUCCESS";
+        case CUBLAS_STATUS_NOT_INITIALIZED:  return "CUBLAS_STATUS_NOT_INITIALIZED";
+        case CUBLAS_STATUS_ALLOC_FAILED:     return "CUBLAS_STATUS_ALLOC_FAILED";
+        case CUBLAS_STATUS_INVALID_VALUE:    return "CUBLAS_STATUS_INVALID_VALUE";
+        case CUBLAS_STATUS_ARCH_MISMATCH:    return "CUBLAS_STATUS_ARCH_MISMATCH";
+        case CUBLAS_STATUS_MAPPING_ERROR:    return "CUBLAS_STATUS_MAPPING_ERROR";
+        case CUBLAS_STATUS_EXECUTION_FAILED: return "CUBLAS_STATUS_EXECUTION_FAILED";
+        case CUBLAS_STATUS_INTERNAL_ERROR:   return "CUBLAS_STATUS_INTERNAL_ERROR";
+        case CUBLAS_STATUS_NOT_SUPPORTED:    return "CUBLAS_STATUS_NOT_SUPPORTED";
+        default:                             return "Unknown cuBLAS error";
+    }
+}
 
 // Helper function to get cuSPARSE error string
 inline const char* cusparseGetErrorString(cusparseStatus_t status) {
